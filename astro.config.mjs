e2b7b901a32569034371755d571d4294239578be
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-01-20 08:10:22 +0900

    init

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/edge";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: vercel()
+});
